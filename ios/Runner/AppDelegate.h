1bffa0396da0237bf7cf429ec6caf06355ef311c
elseAdmin <needforelse@gmail.com>
2020-05-10 02:08:30 +0530

    ios NB

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -1,6 +1,23 @@
 #import <Flutter/Flutter.h>
 #import <UIKit/UIKit.h>
+#import <GNSMessages.h>
 
+static NSString * const kMyAPIKey = @"AIzaSyAjil3RxeOuO62vw8v9C-QbWiOwA1Spg1w";
 @interface AppDelegate : FlutterAppDelegate
+/**
+ * @property
+ * This class lets you check the permission state of Nearby for the app on the current device.  If
+ * the user has not opted into Nearby, publications and subscriptions will not function.
+ */
+@property(nonatomic) GNSPermission *nearbyPermission;
 
+/**
+ * @property
+ * The message manager lets you create publications and subscriptions.  They are valid only as long
+ * as the manager exists.
+ */
+@property(nonatomic) GNSMessageManager *messageMgr;
+@property(nonatomic) id<GNSPublication> publication;
+@property(nonatomic) id<GNSSubscription> subscription;
+@property(nonatomic) UINavigationController *navController;
 @end
